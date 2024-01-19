eaecef5603afcbdf8fb30d57a2493d219e0c8096
gviterig2023 <138837619+gviterig2023@users.noreply.github.com>
2024-01-18 20:45:41 -0500

    Add files via upload

---
+++ Ejercicio17.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        int numeros[] = {30, 1, 18, 5, 23};
+        String letras[] = {"laptop", "cocina", "aves", "zanahoria", "pala"};
+        Arrays.sort(numeros);
+        System.out.println("Arreglo ordenado de números "+ Arrays.toString(numeros));
+        Arrays.sort(letras);
+        System.out.println("Arreglo ordenado de palabras"+ Arrays.toString(letras));
+    }
+}
